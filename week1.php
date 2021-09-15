16b127aeecf97172a7e65e456caff0f6df4ed18f
lintangfauzi <fauzilintang2002@gmail.com>
2021-09-15 14:13:55 +0700

    Initial commit

---
+++ week1.php
@@ -0,0 +1,27 @@
+<?php
+$nama = "Lintang";
+$kelas = "Mi20b";
+$alamat = "singaparna";
+$hoby = "vollyball";
+ ?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title> WEEK 1 - Lintang</title>
+</head>
+<body>
+    <?php
+        echo "BIODATA";
+        echo "<br>"; //untuk memberikan spasi
+        echo "<br>";
+        echo "Nama : ".$nama. "Fauzi";
+        echo "<br>";
+        echo "Kelas : ".$kelas;
+        echo "<br>";
+        echo "Alamat : ".$alamat;
+        echo "<br>";
+        echo "Hoby: ".$hoby;
+        echo "<br>";
+    ?>
+</body>
+</html>
