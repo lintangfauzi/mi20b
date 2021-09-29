d774e471a5e2b7bbdf58bc9f88c562337ed16cde
lintangfauzi <fauzilintang2002@gmail.com>
2021-09-29 14:19:00 +0700

    Create Untitled-php.cu

---
+++ Untitled-php.cu
@@ -0,0 +1,20 @@
+<?php
+error_reporting(0);
+ 
+//DATABASE PERTAMA
+$db_host1 = 'localhost';
+$db_user1 = 'root';
+$db_pass1 = '';
+$database1 = 'lintang';
+ 
+// Buat Koneksinya
+$db1 = new mysqli($db_host1, $db_user1, $db_pass1, $database1);
+ 
+// Cek koneksi *BISA SOBAT HAPUS NANTINYA 
+if ($db1->connect_error) { 
+	die("<b>Yahh! Koneksi Database pertama 'db_dewankomputer' gagal</b> : " . mysqli_connect_error()); 
+} else { 
+	echo "<b>Hore! Koneksi Database pertama 'db_dewankomputer' Berhasil</b>"; 
+}
+ 
+echo "<br><br><br>";
